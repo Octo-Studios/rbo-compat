2be56355650e8ca77e34ad191b4c5b127c72117d
Amiri <iuak228@gmail.com>
2024-06-03 19:07:27 +0300

    new lock ring of Odin

---
+++ src/main/java/it/hurts/sskirillss/rbocompat/events/RingOfOdinItemEvent.java
@@ -0,0 +1,59 @@
+package it.hurts.sskirillss.rbocompat.events;
+
+import it.hurts.sskirillss.rbocompat.mixin.RingOfOdinItemMixin;
+import it.hurts.sskirillss.rbocompat.utils.ManaUtils;
+import it.hurts.sskirillss.relics.init.ItemRegistry;
+import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
+import it.hurts.sskirillss.relics.items.relics.necklace.HolyLocketItem;
+import it.hurts.sskirillss.relics.utils.EntityUtils;
+import it.hurts.sskirillss.relics.utils.NBTUtils;
+import net.minecraft.core.NonNullList;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import vazkii.botania.common.item.BotaniaItems;
+
+@Mod.EventBusSubscriber
+public class RingOfOdinItemEvent {
+
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+        if (!(event.getEntity() instanceof Player player))
+            return;
+
+        ItemStack stack = EntityUtils.findEquippedCurio(player, BotaniaItems.odinRing);
+
+        if (!(stack.getItem() instanceof IRelicItem) || stack.getItem() != BotaniaItems.odinRing || ManaUtils.getTotalMana(player) <= 0)
+            return;
+
+        if (NBTUtils.getBoolean(stack, "toggled", true)) {
+            ManaUtils.consumeMana(player, 20000);
+
+            event.setAmount(event.getAmount() * 0.5F);
+        }
+
+        if (!(NBTUtils.getBoolean(stack, "toggled", true))) {
+            DamageSource source = event.getSource();
+            Entity attacker = source.getEntity();
+            LivingEntity target = event.getEntity();
+
+            if (!(attacker instanceof LivingEntity || target == null))
+                return;
+            float damage = event.getAmount();
+
+            ManaUtils.consumeMana(player, 20000);
+
+            attacker.hurt(source, damage * 0.5F);
+        }
+
+    }
+}
