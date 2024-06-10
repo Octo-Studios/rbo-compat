b5d0d9c0e6524b1688287f615eb8b693f4fb241a
Amiri <iuak228@gmail.com>
2024-06-10 20:00:37 +0300

    Added ability for Thor's ring

--- src/main/java/it/hurts/sskirillss/rbocompat/events/RingOfThorItemEvent.java
+++ src/main/java/it/hurts/sskirillss/rbocompat/events/RingOfThorItemEvent.java
@@ -2,75 +2,18 @@ package it.hurts.sskirillss.rbocompat.events;
 
 import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
 import it.hurts.sskirillss.relics.utils.EntityUtils;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.particles.ParticleTypes;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.BlockTags;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.ServerChatEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import vazkii.botania.common.item.BotaniaItems;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Mod.EventBusSubscriber
 public class RingOfThorItemEvent {
-    private static final TagKey<Block> ORES_TAG = BlockTags.create(new ResourceLocation("forge", "ores"));
-    private static final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
-
     private static int consecutiveBlocksMined = 0;
     private static long lastBlockMinedTime = 0;
-
-    @SubscribeEvent
-    public static void onServerChat(ServerChatEvent event) {
-        ServerPlayer player = event.getPlayer();
-        ServerLevel world = player.serverLevel();
-        BlockPos playerPos = player.blockPosition();
-        int radius = 25;
-        int minY = world.getMinBuildHeight();
-
-        List<CompletableFuture<Void>> futures = new ArrayList<>();
-
-        int chunkSize = 25;
-        for (int x = -radius; x <= radius; x += chunkSize) {
-            for (int z = -radius; z <= radius; z += chunkSize) {
-                int finalX = x;
-                int finalZ = z;
-                futures.add(CompletableFuture.runAsync(() -> scanArea(world, playerPos, finalX, finalZ, chunkSize, minY), executor));
-            }
-        }
-
-        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-    }
-
-    private static void scanArea(ServerLevel world, BlockPos centerPos, int offsetX, int offsetZ, int chunkSize, int minY) {
-        for (int x = offsetX; x < offsetX + chunkSize; x++) {
-            for (int z = offsetZ; z < offsetZ + chunkSize; z++) {
-                for (int y = centerPos.getY(); y >= minY; y--) {
-                    BlockPos pos = centerPos.offset(x, y - centerPos.getY(), z);
-                    BlockState state = world.getBlockState(pos);
-                    Block block = state.getBlock();
-                    if (block.defaultBlockState().is(ORES_TAG)) {
-                        world.sendParticles(ParticleTypes.HAPPY_VILLAGER, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 10, 0.5, 0.5, 0.5, 0.0);
-                    }
-                }
-            }
-        }
-    }
-
 
     @SubscribeEvent
     public static void onBreakBlock(BlockEvent.BreakEvent event) {
--- src/main/java/it/hurts/sskirillss/rbocompat/mixin/RingOfThorItemMixin.java
+++ src/main/java/it/hurts/sskirillss/rbocompat/mixin/RingOfThorItemMixin.java
@@ -3,6 +3,7 @@ package it.hurts.sskirillss.rbocompat.mixin;
 import it.hurts.sskirillss.relics.items.relics.base.IRelicItem;
 import it.hurts.sskirillss.relics.items.relics.base.data.RelicData;
 import it.hurts.sskirillss.relics.items.relics.base.data.cast.CastData;
+import it.hurts.sskirillss.relics.items.relics.base.data.cast.misc.CastStage;
 import it.hurts.sskirillss.relics.items.relics.base.data.cast.misc.CastType;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilitiesData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.AbilityData;
@@ -11,20 +12,33 @@ import it.hurts.sskirillss.relics.items.relics.base.data.leveling.StatData;
 import it.hurts.sskirillss.relics.items.relics.base.data.leveling.misc.UpgradeOperation;
 import it.hurts.sskirillss.relics.utils.MathUtils;
 import it.hurts.sskirillss.relics.utils.NBTUtils;
-import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import vazkii.botania.common.handler.EquipmentHandler;
-import vazkii.botania.common.item.BotaniaItems;
 import vazkii.botania.common.item.relic.RelicBaubleItem;
 import vazkii.botania.common.item.relic.RingOfThorItem;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Mixin(RingOfThorItem.class)
 public class RingOfThorItemMixin extends RelicBaubleItem implements IRelicItem {
+    private static final TagKey<Block> ORES_TAG = BlockTags.create(new ResourceLocation("forge", "ores"));
+    private static final ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+
     public RingOfThorItemMixin(Properties props) {
         super(props);
     }
@@ -56,6 +70,46 @@ public class RingOfThorItemMixin extends RelicBaubleItem implements IRelicItem {
                         .build())
                 .leveling(new LevelingData(100, 10, 100))
                 .build();
+    }
+
+    @Override
+    public void castActiveAbility(ItemStack stack, Player player, String ability, CastType type, CastStage stage) {
+        if (ability.equals("revelation")) {
+            if (player.level().isClientSide) return;
+
+            ServerLevel world = (ServerLevel) player.level();
+            BlockPos playerPos = player.blockPosition();
+            int radius = 30;
+            int minY = world.getMinBuildHeight();
+            int chunkSize = 25;
+
+            for (int x = -radius; x <= radius; x += chunkSize) {
+                for (int z = -radius; z <= radius; z += chunkSize) {
+                    int finalX = x;
+                    int finalZ = z;
+                    CompletableFuture.runAsync(() -> scanArea(world, playerPos, finalX, finalZ, chunkSize, minY), executor)
+                            .exceptionally(ex -> {
+                                ex.printStackTrace();
+                                return null;
+                            });
+                }
+            }
+        }
+    }
+
+    private static void scanArea(ServerLevel world, BlockPos centerPos, int offsetX, int offsetZ, int chunkSize, int minY) {
+        for (int x = offsetX; x < offsetX + chunkSize; x++) {
+            for (int z = offsetZ; z < offsetZ + chunkSize; z++) {
+                for (int y = centerPos.getY(); y >= minY; y--) {
+                    BlockPos pos = centerPos.offset(x, y - centerPos.getY(), z);
+                    BlockState state = world.getBlockState(pos);
+                    Block block = state.getBlock();
+                    if (block.defaultBlockState().is(ORES_TAG)) {
+                        world.sendParticles(ParticleTypes.HAPPY_VILLAGER, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, 10, 0.5, 0.5, 0.5, 0.0);
+                    }
+                }
+            }
+        }
     }
 
 
